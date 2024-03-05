40c088d6a95d0dcbaddd76cc677b54a6265e45a0
Fabio Semeraro <semef2@bfh.ch>
2024-03-05 09:51:55 +0100

    add new vector type

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include <string>
+
+struct Vector 2d{
+	float x{0};
+	float y{0};
+	float z{0};
+};
 
 int main()
 {
 	std::cout << "Hello C++" << std::endl;
+
+	Vector2d myvec{1.f, 2.f, 3.f};
 }
 
 //EOF
