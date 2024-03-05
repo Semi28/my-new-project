b702e45b7c470f423bcafb70eb4049f9ad63f1e1
Fabio Semeraro <semef2@bfh.ch>
2024-03-05 09:56:19 +0100

    rename objects

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,16 @@
 struct Vector 2d{
 	float x{0};
 	float y{0};
-	float z{0};
+
 };
 
 int main()
 {
 	std::cout << "Hello C++" << std::endl;
 
+	Vector2d myvec{1.f, 2.f, 3.f};       
 	Vector2d myvec{1.f, 2.f, 3.f};
+
 }
 
 //EOF
